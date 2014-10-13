b461e6b5e562410b182317b77d1d536105ab7740
Michael Zhang <zhangmichael@berkeley.edu>
2014-10-13 16:55:12 -0700

    initial commit

--- rsa.py
+++ rsa.py
@@ -1,11 +1,9 @@
-def generateKey(a, b, k=7):
-
+def genKey(a, b, k=7):
 	try:
-
-		p = gen_prime(a, b)
+		p = genPrime(a, b)
 		q = p
 		while q == p:
-			q = gen_prime(a, b)
+			q = genPrime(a, b)
 	except:
 		raise ValueError
 
@@ -16,5 +14,6 @@ def generateKey(a, b, k=7):
 		while gcd(e, m) != 1:
 			e = random.randint(3, m-1)
 		d = inverse(e, m)
-
 	return (N, e, d)
+def genPrime(min, max):
+	return
